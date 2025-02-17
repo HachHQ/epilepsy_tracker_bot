2a74236698276992c4a2f929f998db3fccf151a7
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-02-17 19:51:45 +0700

    Return main menu module

---
+++ handlers/main_menu.py
@@ -0,0 +1,37 @@
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters.callback_data import CallbackData
+from aiogram.filters import Command
+
+
+from database.db_init import SessionLocal
+from database.models import User
+
+from keyboards.menu_kb import get_main_menu_keyboard
+
+from services.notification_queue import get_notification_queue
+
+main_menu_router = Router()
+
+class MenuCallbackFactory(CallbackData, prefix=":"):
+    action: str
+    value: str
+
+
+
+@main_menu_router.message(Command(commands="menu"))
+async def send_main_menu(message: Message):
+    await message.answer(
+        f"Вы находитесь в основном меню бота.\n"
+        "Используйте кнопки для навигации.\n",
+        reply_markup=get_main_menu_keyboard()
+    )
+
+@main_menu_router.callback_query(F.data == "to_menu")
+async def send_main_menu_callback(callback: CallbackQuery):
+    await callback.message.answer(
+        f"Вы находитесь в основном меню бота.\n"
+        "Используйте кнопки для навигации.\n",
+        reply_markup=get_main_menu_keyboard()
+    )
+    await callback.answer()
