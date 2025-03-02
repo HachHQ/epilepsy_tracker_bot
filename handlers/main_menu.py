870944c820f32e5b5176edba7318d818860def60
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-02 17:05:45 +0700

    Rewrite login view in main menu to async version of alchemy engine and start using update login cache functions

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -2,19 +2,14 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters.callback_data import CallbackData
 from aiogram.filters import Command
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
-from database.db_init import SessionLocal
 from database.models import User
 from database.redis_client import redis
-
 from services.notification_queue import NotificationQueue
-
+from services.update_login_cache import get_cached_login, set_cached_login
 from keyboards.menu_kb import get_main_menu_keyboard
-
-# from services.notification_queue import get_notification_queue
 
 main_menu_router = Router()
 
@@ -22,40 +17,46 @@ main_menu_router = Router()
 #     action: str
 #     value: str
 
-async def get_user_login(message) -> str:
-    user_id = message.chat.id
-    login = None
-    # Пытаемся получить логин из Redis
-    login_bytes = await redis.get(f"user:login:{user_id}")
-    print(login_bytes)
-    if login_bytes:
-        login = login_bytes.decode('utf-8')
-    print(login)
-    if not login:
-        # Если нет в кэше, достаем из БД
-        async with SessionLocal() as db:
-            user = db.query(User).filter(User.telegram_id == user_id).first()
-            db.close()
+# async def get_user_login(message) -> str:
+#     user_id = message.chat.id
+#     login = None
+#     login_bytes = await redis.get(f"user:login:{user_id}")
+#     print(login_bytes)
+#     if login_bytes:
+#         login = login_bytes.decode('utf-8')
+#     print(login)
+#     if not login:
+#         async with SessionLocal() as db:
+#             user = db.query(User).filter(User.telegram_id == user_id).first()
 
-            if user:
-                login = user.login
-                await redis.setex(f"user:login:{user_id}", 300, login)  # Сохраняем в кэше
+#             if user:
+#                 login = user.login
+#                 await redis.setex(f"user:login:{user_id}", 300, login)
 
-    return login or "Логин не найден"
+#     return login or "Логин не найден"
 
 @main_menu_router.message(Command(commands="menu"))
-async def send_main_menu(message: Message):
+async def send_main_menu(message: Message, db: AsyncSession):
+    result = await db.execute(select(User).filter(User.telegram_id == message.chat.id))
+    user_login = result.scalars().first()
+    await set_cached_login(message.chat.id, user_login.login)
+    lg = await get_cached_login(message.chat.id)
+    print(lg)
     await message.answer(
-        f"Логин: {await get_user_login(message)}\n"
+        f"Логин: {lg}\n"
         f"Вы находитесь в основном меню бота.\n"
         "Используйте кнопки для навигации.\n",
         reply_markup=get_main_menu_keyboard()
     )
 
 @main_menu_router.callback_query(F.data == "to_menu")
-async def send_main_menu_callback(callback: CallbackQuery):
+async def send_main_menu_callback(callback: CallbackQuery, db: AsyncSession):
+    result = await db.execute(select(User).filter(User.login == callback.message.chat.id))
+    user_login = result.scalars().first()
+    print(user_login)
+    set_cached_login(callback.message.chat.id, user_login)
     await callback.message.answer(
-        f"Логин: {await get_user_login(callback.message)}\n"
+        f"Логин: {get_cached_login()}\n"
         f"Вы находитесь в основном меню бота.\n"
         "Используйте кнопки для навигации.\n",
         reply_markup=get_main_menu_keyboard()
@@ -65,7 +66,7 @@ async def send_main_menu_callback(callback: CallbackQuery):
 @main_menu_router.message(F.text.startswith('send_'))
 async def send_notification_someone(message: Message, notification_queue: NotificationQueue, db: AsyncSession):
     try:
-        login = message.text.split("_", 1)[1]  # Разделяем 'send_логин' → получаем логин
+        login = message.text.split("_", 1)[1]
         print(f"Поиск пользователя с логином: {login}")
 
         result = await db.execute(select(User).filter(User.login == login))
--- services/update_login_cache.py
+++ services/update_login_cache.py
@@ -1,9 +1,8 @@
 from database.redis_client import redis
 
 async def get_cached_login(user_id: int) -> str:
-    """Получает логин из кэша или None, если логина нет"""
-    return await redis.get(f"user:login:{user_id}")
+    login = await redis.get(f"user:login:{user_id}")
+    return login.decode("utf-8") if login else None
 
 async def set_cached_login(user_id: int, login: str):
-    """Сохраняет логин пользователя в Redis на 5 минут"""
     await redis.setex(f"user:login:{user_id}", 300, login)
