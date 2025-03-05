1b17dd30fc4d408c09b06545858cf44c7c79dd5e
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-05 18:58:55 +0700

    Add seizure form router

---
+++ handlers/seizures_handlers.py
@@ -0,0 +1,72 @@
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
+from aiogram.filters.callback_data import CallbackData
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State, default_state
+from aiogram.filters import Command, StateFilter
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.exc import SQLAlchemyError
+
+from services.update_login_cache import get_cached_login
+from database.models import User, Profile
+from keyboards.menu_kb import get_main_menu_keyboard
+
+
+
+seizures_router = Router()
+
+# class SeizureForm(StatesGroup):
+#     date =
+#     time =
+#     triggers =
+#     severity =
+#     duration =
+#     comment =
+#     count =
+#     symptoms =
+
+#     video_tg_id =
+#     created_at =
+#     updated_at =
+#     location =
+
+@seizures_router.callback_query(F.data == "menu:fix_attack")
+async def offer_to_choose_profile(callback: CallbackQuery, db: AsyncSession):
+    redis_login = await get_cached_login(callback.message.chat.id)
+    try:
+        query = (
+            select(Profile)
+            .join(User)
+            .where(User.login == redis_login)
+        )
+        profiles_result = await db.execute(query)
+        profiles = profiles_result.scalars().all()
+
+        back_btn = InlineKeyboardButton(text="⬅️ Назад", callback_data="back:to_menu")
+        profiles_kb_bd = InlineKeyboardBuilder()
+        for profile in profiles:
+            profiles_kb_bd.button(text=f"{profile.profile_name}", callback_data=f"seizue_profile:{profile.id}")
+        profiles_kb_bd.adjust(1)
+        profiles_kb_bd.row(back_btn, width=1)
+
+        await callback.message.edit_text("Выберите профиль для которого хотите зафиксировать приступ: ", reply_markup=profiles_kb_bd.as_markup())
+    except SQLAlchemyError as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+        profiles = []
+        await callback.answer()
+
+@seizures_router.callback_query(F.data.startswith("back"))
+async def back_to(callback: CallbackQuery):
+    _, back_to_target = callback.data.split(":", 1)
+
+    if back_to_target == "to_menu":
+        redis_login = await get_cached_login(callback.message.chat.id)
+        await callback.message.edit_text(
+            f"Логин: {redis_login}\n"
+            f"Вы находитесь в основном меню бота.\n"
+            "Используйте кнопки для навигации.\n",
+            reply_markup=get_main_menu_keyboard()
+        )
+    await callback.answer()
