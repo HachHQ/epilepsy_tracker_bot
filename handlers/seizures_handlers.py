4c29beb372eb7e0f2b7a8b93a0439f450883e678
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-06 19:42:06 +0700

    Add some handlers for submenu of seizure script

--- handlers/seizures_handlers.py
+++ handlers/seizures_handlers.py
@@ -12,28 +12,30 @@ from sqlalchemy.exc import SQLAlchemyError
 from services.update_login_cache import get_cached_login
 from database.models import User, Profile
 from keyboards.menu_kb import get_main_menu_keyboard
-from keyboards.seizure_kb import get_year_date_kb
+from keyboards.seizure_kb import get_year_date_kb, get_profiles_for_seizure_fix, get_month_date_kb
 
 
 seizures_router = Router()
 
 class SeizureForm(StatesGroup):
     date = State()
-    time = State()
+    year = State()
+    month = State()
+    hour = State()
+    minutes_range = State()
+    count = State()
     triggers = State()
     severity = State()
     duration = State()
     comment = State()
-#     count =
-#     symptoms =
-
-#     video_tg_id =
+    symptoms = State()
+    video_tg_id = State()
 #     created_at =
 #     updated_at =
 #     location =
 
 @seizures_router.callback_query(F.data.startswith("back"))
-async def back_to(callback: CallbackQuery):
+async def back_to(callback: CallbackQuery, state: FSMContext):
     _, back_to_target = callback.data.split(":", 1)
 
     if back_to_target == "to_menu":
@@ -44,6 +46,12 @@ async def back_to(callback: CallbackQuery):
             "Используйте кнопки для навигации.\n",
             reply_markup=get_main_menu_keyboard()
         )
+    elif back_to_target == "year":
+        data = await state.get_data()
+        await callback.message.edit_text(f"Выбран профиль - {data['profile_name']}\nВыберите год или сразу день из преложенных",
+                                            reply_markup=get_year_date_kb(3,1))
+        return
+    await state.clear()
     await callback.answer()
 
 @seizures_router.callback_query(F.data == "menu:choose_profile")
@@ -57,27 +65,40 @@ async def offer_to_choose_profile(callback: CallbackQuery, db: AsyncSession):
         )
         profiles_result = await db.execute(query)
         profiles = profiles_result.scalars().all()
-
-        back_btn = InlineKeyboardButton(text="⬅️ Назад", callback_data="back:to_menu")
-        profiles_kb_bd = InlineKeyboardBuilder()
-        i=0
-        for profile in profiles:
-            i += 1
-            profiles_kb_bd.button(text=f"{i} - {profile.profile_name}", callback_data=f"fix_seizure:{profile.id}")
-        profiles_kb_bd.adjust(1)
-        profiles_kb_bd.row(back_btn, width=1)
-
-        await callback.message.edit_text("Выберите профиль для которого хотите зафиксировать приступ: ", reply_markup=profiles_kb_bd.as_markup())
+        await callback.message.edit_text("Выберите профиль для которого хотите зафиксировать приступ: ", reply_markup=get_profiles_for_seizure_fix(profiles))
     except SQLAlchemyError as e:
         print(f"Ошибка при выполнении запроса: {e}")
-        profiles = []
         await callback.answer()
 
-#TODO set a kb to this handler with years
 @seizures_router.callback_query(F.data.startswith("fix_seizure"))
 async def start_fix_seizure(callback: CallbackQuery, state: FSMContext):
-    _, profile_id = callback.data.split(":", 1)
+    print("не кнопка назад")
+    _, profile_id, profile_name = callback.data.split(":", 2)
     await state.update_data(profile_id=profile_id)
+    await state.update_data(profile_name=profile_name)
+    await callback.message.answer(f"Выбран профиль - {profile_name}\nВыберите год или сразу день из преложенных",
+                                        reply_markup=get_year_date_kb(3,1))
+    await callback.answer()
 
-    await callback.message.edit_text(f"Выбран профиль - {profile_id}\nВыберите год или день из преложенных",
-                                    reply_markup=get_year_date_kb(3,1))
+@seizures_router.callback_query(F.data.startswith("year"))
+async def process_date_short(callback: CallbackQuery, state: FSMContext):
+    _, year = callback.data.split(":", 1)
+    if year == "two_d_ago":
+        await state.update_data(date_short=year)
+        await state.set_state(SeizureForm.hour)
+        await callback.message.edit_text()
+        return
+    elif year == "one_d_ago":
+        await state.update_data(date_short=year)
+        await state.set_state(SeizureForm.hour)
+        await callback.message.edit_text()
+        return
+    elif year == "today":
+        await state.update_data(date_short=year)
+        await state.set_state(SeizureForm.hour)
+        await callback.message.edit_text()
+        return
+    else:
+        await state.update_data(year=year)
+        await state.set_state(SeizureForm.month)
+        await callback.message.edit_text(f"Выбран {year} год.\nВыберите месяц:", reply_markup=get_month_date_kb())
