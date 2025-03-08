82e972f0e092319d0c794c63aba9df89a27f572b
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-08 22:21:24 +0700

    Update get login func, add check for exist of login

--- services/update_login_cache.py
+++ services/update_login_cache.py
@@ -12,13 +12,20 @@ async def get_cached_login(user_id: int) -> str:
         print("from redis")
         return login.decode('utf-8')
     if login == None:
-        async with SessionLocal() as db:
-            result = await db.execute(select(User).filter(User.telegram_id == user_id))
-            user = result.scalars().first()
-            login = user.login
-        await set_cached_login(user_id, login)
-        print("from db")
-        return login
+        try:
+            async with SessionLocal() as db:
+                result = await db.execute(select(User).filter(User.telegram_id == user_id))
+                user = result.scalars().first()
+                if not user:
+                    print("Пользователь не зарегестрирован")
+                    login = "Не зарегистрирован"
+                    return
+                login = user.login
+            await set_cached_login(user_id, login)
+            print("from db")
+            return login
+        except Exception as e:
+            print(f"Неизвестная ошибка: {e}")
 
 async def set_cached_login(user_id: int, login: str):
     await redis.setex(f"user:login:{user_id}", 3600, login)
