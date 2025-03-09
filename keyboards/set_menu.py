ebd951ba6c7a87e0833d317ae769de2240b5e366
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-09 17:46:47 +0700

    Add module for profile choosing command

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -3,11 +3,9 @@ from aiogram.types import BotCommand
 
 async def set_main_menu(bot: Bot):
     main_menu_commands = [
-        BotCommand(command='/start',
-                   description='Начать работу с ботом / Заполнить анкету заново'),
-        BotCommand(command='/menu',
-                   description='Вызвать основное меню'),
-        BotCommand(command='/help',
-                   description='Узнать о возможностях бота'),
+        BotCommand(command='/menu', description='Вызвать основное меню'),
+        BotCommand(command='/profile', description='Выбрать профиль по умолчанию'),
+        BotCommand(command='/start', description='Начать работу с ботом / Заполнить анкету заново'),
+        BotCommand(command='/help', description='Узнать о возможностях бота'),
     ]
     await bot.set_my_commands(main_menu_commands)
