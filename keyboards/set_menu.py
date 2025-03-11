af4b6afb70cc6248ead659f72354a904df0eb977
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-11 14:49:31 +0700

    Edit text for /start command

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -5,7 +5,7 @@ async def set_main_menu(bot: Bot):
     main_menu_commands = [
         BotCommand(command='/menu', description='Вызвать основное меню'),
         BotCommand(command='/profile', description='Выбрать профиль по умолчанию'),
-        BotCommand(command='/start', description='Начать работу с ботом / Заполнить анкету заново'),
+        BotCommand(command='/start', description='Начать работу с ботом'),
         BotCommand(command='/help', description='Узнать о возможностях бота'),
     ]
     await bot.set_my_commands(main_menu_commands)
