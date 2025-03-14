8f9f48106e22388020b51c8d97f5f27b33cc63a2
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-14 19:50:11 +0700

    Remove /profiles command

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -4,7 +4,6 @@ from aiogram.types import BotCommand
 async def set_main_menu(bot: Bot):
     main_menu_commands = [
         BotCommand(command='/menu', description='Вызвать основное меню'),
-        BotCommand(command='/profile', description='Выбрать профиль по умолчанию'),
         BotCommand(command='/start', description='Начать работу с ботом'),
         BotCommand(command='/help', description='Узнать о возможностях бота'),
     ]
