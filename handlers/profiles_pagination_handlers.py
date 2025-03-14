9d30885ea12f9f28746bf79edcd838727a19d176
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-03-14 15:47:47 +0700

    Add universal handler for process of pagination

---
+++ handlers/profiles_pagination_handlers.py
@@ -0,0 +1,26 @@
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+
+from services.redis_cache_data import get_cached_profiles_list
+from keyboards.profiles_list_kb import get_paginated_profiles_kb
+
+pagination_router = Router()
+
+@pagination_router.callback_query((F.data.startswith('prev')) | (F.data.startswith('next')))
+async def handle_pagination(callback: CallbackQuery):
+    direction, page, profile_type = callback.data.split(':', 2)
+    page = int(page)
+    profiles_redis = await get_cached_profiles_list(callback.message.chat.id, profile_type)
+    if not profiles_redis:
+        await callback.message.answer("Ошибка: Список профилей не найден.")
+        return
+    new_page = max(0, page - 1) if direction == "prev" else min(len(profiles_redis) // 5, page + 1)
+    await callback.message.edit_text(
+        "Выберите профиль:",
+        reply_markup=get_paginated_profiles_kb(
+            profiles=profiles_redis,
+            page=new_page,
+            profile_type=profile_type
+        )
+    )
+    await callback.answer()
