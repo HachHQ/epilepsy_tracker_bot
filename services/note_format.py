e3df753d85bf828effd1cf7ca85ed098c8e11ba3
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-04-09 18:51:47 +0700

    Update methods for display seizure data in different ways

--- services/note_format.py
+++ services/note_format.py
@@ -1,53 +1,60 @@
 def get_formatted_seizure_info(
-    callback,
     current_profile,
-    date = 'Не заполнено',
-    time_of_day = 'Не заполнено',
-    count = 'Не заполнено',
-    triggers = 'Не заполнено',
-    severity = 'Не заполнено',
-    duration = 'Не заполнено',
-    comment = 'Не заполнено',
-    symptoms = 'Не заполнено',
-    video_tg_id = 'Не заполнено',
-    location = 'Не заполнено'
+    date,
+    time,
+    count,
+    triggers,
+    severity,
+    duration,
+    comment,
+    symptoms,
+    video_tg_id,
+    location,
+    seizure_id: int = 0
 ):
-    text = (f"Введенные данные о приступе для профиля <u>{current_profile}</u>:\n"
-        f"Дата: {date}\n"
-        f"Время: {time_of_day}\n"
-        f"Количество: {count}\n"
-        f"Триггеры: {triggers}\n"
-        f"Тяжесть: {severity} баллов\n"
-        f"Продолжительность: {duration} минут\n"
-        f"Комментарий: {comment}\n"
-        f"Симптомы: {symptoms}\n"
-        f"Видео: {"✅" if video_tg_id != "Не заполнено" else video_tg_id}\n"
-        f"Место: {location}")
+    action_lines = (
+        f"\n________________________________________"
+        f"\n\nРедактировать запись: /edit_{seizure_id}\n\n"
+        f"Удалить запись: /delete_{seizure_id}"
+    )
+    text = (f"Данные о приступе для профиля <u>{current_profile}</u>:\n\n"
+        f"Дата: {date if date else "Не введено"}\n"
+        f"Время: {time if time else "Не введено"}\n"
+        f"Количество: {count if count else "Не введено"}\n"
+        f"Триггеры: {triggers if triggers else "Не введено"}\n"
+        f"Тяжесть: {str(severity) + ' баллов ' if severity else "Не введено"}\n"
+        f"Продолжительность: {str(duration) + ' минут ' if duration else "Не введено"}\n"
+        f"Комментарий: {comment if comment else "Не введено"}\n"
+        f"Симптомы: {symptoms if symptoms else "Не введено"}\n"
+        f"Видео: {"✅" if video_tg_id else '❌'}\n"
+        f"Место: {"✅" if location else "❌"}"
+        f"{action_lines if seizure_id > 0 else ""}"
+    )
     return text
 
 def get_formatted_seizure_edit(
-    current_profile,
     seizure_id,
-    date = 'Не заполнено',
-    time_of_day = 'Не заполнено',
-    count = 'Не заполнено',
-    triggers = 'Не заполнено',
-    severity = 'Не заполнено',
-    duration = 'Не заполнено',
-    comment = 'Не заполнено',
-    symptoms = 'Не заполнено',
-    video_tg_id = 'Не заполнено',
-    location = 'Не заполнено'
+    current_profile,
+    date,
+    time,
+    count,
+    triggers,
+    severity,
+    duration,
+    comment,
+    symptoms,
+    video_tg_id,
+    location
 ):
-    text = (f"Введенные данные о приступе для профиля <u>{current_profile}</u>:\n"
-        f"Дата: {date} /edit_date{seizure_id}\n"
-        f"Время: {time_of_day} /edit_time{seizure_id}\n"
-        f"Количество: {count} /edit_count{seizure_id}\n"
-        f"Триггеры: {triggers} /edit_triggers{seizure_id}\n"
-        f"Тяжесть: {severity} /edit_severity{seizure_id}\n"
-        f"Продолжительность: {duration} минут /edit_duration{seizure_id}\n"
-        f"Комментарий: {comment} /edit_comment{seizure_id}\n"
-        f"Симптомы: {symptoms} /edit_symptoms{seizure_id}\n"
-        f"Видео: {"✅" if video_tg_id != "Не заполнено" else video_tg_id} /edit_video{seizure_id}\n"
-        f"Место: {location} /edit_location{seizure_id}")
+    text = (f"Данные о приступе для профиля <u>{current_profile}</u>:\n\n"
+        f"Дата: {date if date else "Не введено"} /update_date{seizure_id}\n\n"
+        f"Время: {time if time else "Не введено"} /update_time_{seizure_id}\n\n"
+        f"Количество: {count if count else "Не введено"} /update_count_{seizure_id}\n\n"
+        f"Триггеры: {triggers if triggers else "Не введено"} /update_triggers_{seizure_id}\n\n"
+        f"Тяжесть: {str(severity) + ' баллов ' if severity else "Не введено"} /update_severity_{seizure_id}\n\n"
+        f"Продолжительность: {str(duration) + ' минут ' if duration else "Не введено"} /update_duration_{seizure_id}\n\n"
+        f"Комментарий: {comment if comment else "Не введено"} /update_comment_{seizure_id}\n\n"
+        f"Симптомы: {symptoms if symptoms else "Не введено"} /update_symptoms_{seizure_id}\n\n"
+        f"Видео: {"✅" if video_tg_id else '❌'} /update_video_{seizure_id}\n\n"
+        f"Место: {"✅" if location else "❌"} /update_location_{seizure_id}")
     return text
