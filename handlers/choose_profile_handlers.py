d17bb0538456074e55bd85f31234e725b8b49c66
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-04-09 16:43:32 +0700

    Minor updates

--- handlers/choose_profile_handlers.py
+++ handlers/choose_profile_handlers.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from database.models import User, Profile
+from database.orm_query import orm_get_user, orm_get_profile_by_id
 from database.redis_query import set_redis_cached_current_profile
 from services.redis_cache_data import get_cached_profiles_list
 from keyboards.profiles_list_kb import get_choosing_type_of_profiles_kb, get_paginated_profiles_kb
@@ -38,10 +39,8 @@ async def select_own_profile(callback: CallbackQuery, db: AsyncSession):
 @choose_profile_router.callback_query((F.data.startswith('select_profile')) & ~(F.data.endswith('|share')))
 async def process_choosing_of_profile(callback: CallbackQuery, db: AsyncSession):
     _, profile_id, profile_name = callback.data.split(':', 2)
-    search_user = await db.execute(select(User).filter(User.telegram_id == callback.message.chat.id))
-    user = search_user.scalars().first()
-    search_profile = await db.execute(select(Profile).filter(Profile.id == int(profile_id)))
-    profile = search_profile.scalars().first()
+    user = await orm_get_user(db, callback.message.chat.id)
+    profile = await orm_get_profile_by_id(db, int(profile_id))
     if not profile:
         await callback.message.answer("Такой профиль не существует.")
     user.current_profile = profile.id
