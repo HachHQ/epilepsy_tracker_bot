8f6496c3718218439c10fade3b34dcbf534038f1
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-04-20 11:44:16 +0700

    Add praphs and stats commands

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -4,7 +4,9 @@ from aiogram.types import BotCommand
 async def set_main_menu(bot: Bot):
     main_menu_commands = [
         BotCommand(command='/menu', description='Вызвать основное меню'),
-        BotCommand(command='/start', description='Начать работу с ботом'),
+        BotCommand(command='/graphs', description='Вызвать меню для графиков'),
+        BotCommand(command='/stats', description='Вызвать меню для статистики'),
         BotCommand(command='/help', description='Узнать о возможностях бота'),
+        BotCommand(command='/start', description='Начать работу с ботом'),
     ]
     await bot.set_my_commands(main_menu_commands)
