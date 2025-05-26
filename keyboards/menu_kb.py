9c4f23b45f5e0b26883ed9761fb38e4f5ba70481
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-05-26 12:11:20 +0700

    minor updates

--- keyboards/menu_kb.py
+++ keyboards/menu_kb.py
@@ -1,5 +1,5 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from lexicon.lexicon import LEXICON_BUTTONS
 
@@ -13,13 +13,13 @@ def get_main_menu_keyboard() -> InlineKeyboardMarkup:
     panic_btn = InlineKeyboardButton(text="🆘 Аура", callback_data="aura_notification")
     fix_btn = InlineKeyboardButton(text="✏️ Зафиксировать приступ", callback_data="fix_seizure")
     journal_btn = InlineKeyboardButton(text="🗂️ Данные о приступах", callback_data="seizure_data")
-    profiles_btn = InlineKeyboardButton(text="👤 Профили", callback_data="choose_profile")
-    add_trusted_person_btn = InlineKeyboardButton(text="🔗 Добавить ДЛ",callback_data="add_trusted")
+    profiles_btn = InlineKeyboardButton(text="👤 Выбрать профиль", callback_data="choose_profile")
+    control_profiles = InlineKeyboardButton(text="⚙️ Управление",callback_data="control_profiles")
     notification_btn = InlineKeyboardButton(text="🔔 Уведомления", callback_data="set_notifications")
-    import_btn = InlineKeyboardButton(text="⬇️ Импортировать", callback_data="import_log")
+    import_btn = InlineKeyboardButton(text="🗄️ Импортировать", callback_data="import_log")
     kb_builder.row(panic_btn)
     kb_builder.row(fix_btn)
     kb_builder.row(profiles_btn, notification_btn)
     kb_builder.row(journal_btn)
-    kb_builder.row(add_trusted_person_btn, import_btn)
+    kb_builder.row(control_profiles, import_btn)
     return kb_builder.as_markup()
