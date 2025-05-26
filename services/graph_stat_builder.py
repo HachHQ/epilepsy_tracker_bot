b9c99de7fcc7fbc5caa00d9cc4da3c1940e4e590
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-05-26 12:16:24 +0700

    now stats processes by the user local time

--- services/graph_stat_builder.py
+++ services/graph_stat_builder.py
@@ -6,19 +6,21 @@ import calendar
 from aiogram.types import FSInputFile, Message
 from sqlalchemy.ext.asyncio import AsyncSession
 from pathlib import Path
-from collections import defaultdict
 from datetime import datetime, timezone, timedelta
 from matplotlib import rcParams
+from matplotlib.ticker import MaxNLocator
 
 Path("temp_images").mkdir(exist_ok=True)
 
+from services.note_format import get_minutes_and_seconds
 from database.orm_query import (
     orm_get_seizures_by_profile_ascending,
     orm_get_seizures_by_profile_descending,
-    orm_get_seizures_for_a_specific_period
+    orm_get_seizures_for_a_specific_period,
 )
 from services.redis_cache_data import (
-    get_cached_current_profile
+    get_cached_current_profile,
+    get_user_local_datetime
 )
 
 month_in_russian = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
@@ -42,7 +44,10 @@ def make_a_gist(
     plt.ylabel(label_y, fontproperties='Lato', fontsize=14)
     plt.title(title, fontproperties='Lato', fontsize=18)
     plt.xticks(ticks=dataX, labels=xticks, fontsize=10)
-    plt.grid(visible=True)
+    ax = plt.gca()
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.ylim(0, 20)
+    plt.grid(visible=True, which='both', linestyle='--', linewidth=0.5)
     return plt.bar(dataX, dataY)
 
 async def get_year_gist(session: AsyncSession, message: Message):
@@ -52,9 +57,10 @@ async def get_year_gist(session: AsyncSession, message: Message):
     seizure_data = await orm_get_seizures_for_a_specific_period(
         session,
         int(current_profile.split('|', 1)[0]),
-        365
+        current_year
     )
-    if seizure_data is None:
+    if len(seizure_data) == 0:
+        await message.answer("Данных для отображения нет.")
         return
     month_arr = range(1, 13)
     count_seizures_by_month = [0] * 12
@@ -75,20 +81,24 @@ async def get_year_gist(session: AsyncSession, message: Message):
     file = FSInputFile(path=path)
     await message.answer_photo(
         photo=file,
-        caption=f"Гистограмма распределения приступов по месяцам в течении {current_year}"
+        caption=f"Гистограмма распределения приступов по месяцам в течение {current_year}"
         f" года для профиля {current_profile.split('|', 1)[1]}",
     )
     os.remove(path)
 
 async def get_month_gist(session: AsyncSession, message: Message):
     days_range = calendar.monthrange(current_year, current_month)[1]
+    print(days_range)
     days_range = [day for day in range(1, days_range + 1)]
+    print(days_range)
     count_seizures_by_days = [0] * len(days_range)
     current_profile = await get_cached_current_profile(session, message.chat.id)
     if current_profile is None:
         return await message.answer("Выберите профиль.")
-    seizures_data = await orm_get_seizures_for_a_specific_period(session, int(current_profile.split('|')[0]), len(days_range))
-    if seizures_data is None:
+    seizures_data = await orm_get_seizures_for_a_specific_period(session, int(current_profile.split('|')[0]), current_year, current_month)
+    print(len(seizures_data))
+    if len(seizures_data) == 0:
+        await message.answer("Данных для отображения нет.")
         return
     for seizure in seizures_data:
         date = datetime.strptime(seizure.date, "%Y-%m-%d")
@@ -119,7 +129,7 @@ async def get_total_seizure_count(session: AsyncSession, message: Message):
     if current_profile is None:
         return await message.answer("Выберите профиль.")
     seizures_data = await orm_get_seizures_by_profile_ascending(session, int(current_profile.split('|')[0]))
-    if len(seizures_data) == 0:
+    if len(seizures_data) == 0 or len(seizures_data) < 2:
         return None
     return len(seizures_data)
 
@@ -128,73 +138,96 @@ async def get_day_without_seizures(session: AsyncSession, message: Message):
     if current_profile is None:
         return await message.answer("Выберите профиль.")
     seizures_data = await orm_get_seizures_by_profile_descending(session, int(current_profile.split('|')[0]))
-    if len(seizures_data) == 0:
+    if len(seizures_data) == 0 or len(seizures_data) < 2:
         return None
     last_date_seizure = datetime.strptime(seizures_data[0].date, "%Y-%m-%d")
-    diff = current_date.date() - last_date_seizure.date()
+    local_date = await get_user_local_datetime(session, message.chat.id)
+    diff = local_date.date() - last_date_seizure.date()
     return diff.days
 
-async def get_avg_duration_of_seizure(session: AsyncSession, message: Message) -> int:
+async def get_avg_duration_of_seizure(session: AsyncSession, message: Message) -> str:
     current_profile = await get_cached_current_profile(session, message.chat.id)
     if current_profile is None:
         return await message.answer("Выберите профиль.")
     seizures_data = await orm_get_seizures_by_profile_ascending(session, int(current_profile.split('|')[0]))
-    if len(seizures_data) == 0:
+    if len(seizures_data) == 0 or len(seizures_data) < 2:
         return None
     seizures_with_duration = 0
-    total_duration = 0
+    total_duration_in_sec = 0
     for seizure in seizures_data:
         if seizure.duration:
             seizures_with_duration += 1
-            total_duration += seizure.duration
+            total_duration_in_sec += seizure.duration
     if seizures_with_duration == 0:
         return None
-    return total_duration // seizures_with_duration
+    return get_minutes_and_seconds(total_duration_in_sec // seizures_with_duration)
 
 async def get_min_max_duration_of_seizure(session: AsyncSession, message: Message) -> str:
     current_profile = await get_cached_current_profile(session, message.chat.id)
     if current_profile is None:
         return await message.answer("Выберите профиль.")
     seizures_data = await orm_get_seizures_by_profile_ascending(session, int(current_profile.split('|')[0]))
-    if len(seizures_data) == 0:
+    if len(seizures_data) == 0 or len(seizures_data) < 2:
         return None
     duration_list = []
     for seizure in seizures_data:
         if seizure.duration:
             duration_list.append(seizure.duration)
 
-    return f"{min(duration_list)}|{max(duration_list)}"
+    return f"{get_minutes_and_seconds(min(duration_list))}|{get_minutes_and_seconds(max(duration_list))}"
 
-async def get_avg_duration_of_seizure_in_a_week(session: AsyncSession, message: Message) -> int:
+async def get_avg_duration_of_seizure_in_a_week(session: AsyncSession, message: Message) -> str:
     current_profile = await get_cached_current_profile(session, message.chat.id)
+    local_date = await get_user_local_datetime(session, message.chat.id)
+    start_of_week = local_date - timedelta(days=(local_date.isoweekday() - 1))
+    print("Понедельник", start_of_week.year, start_of_week.month, start_of_week.day)
+    print(local_date)
     if current_profile is None:
         return await message.answer("Выберите профиль.")
-    seizures_data = await orm_get_seizures_for_a_specific_period(session, int(current_profile.split('|')[0]), 7)
-    if len(seizures_data) == 0:
-        return None
+    seizures_data = await orm_get_seizures_for_a_specific_period(session, int(current_profile.split('|')[0]), start_of_week.year, start_of_week.month, start_of_week.day)
+    if len(seizures_data) < 2:
+        return 0
+    print("Приступов за неделю", len(seizures_data))
     seizures_with_duration = 0
-    total_duration = 0
+    total_duration_in_sec = 0
     for seizure in seizures_data:
         if seizure.duration:
             seizures_with_duration += 1
-            total_duration += seizure.duration
+            total_duration_in_sec += seizure.duration
     if seizures_with_duration == 0:
-        return None
-    return total_duration // seizures_with_duration
+        return 0
+    return get_minutes_and_seconds(total_duration_in_sec // seizures_with_duration)
 
-async def get_avg_duration_of_seizure_in_a_month(session: AsyncSession, message: Message) -> int:
+async def get_avg_duration_of_seizure_in_a_month(session: AsyncSession, message: Message) -> str:
     current_profile = await get_cached_current_profile(session, message.chat.id)
+    local_date = await get_user_local_datetime(session, message.chat.id)
     if current_profile is None:
         return await message.answer("Выберите профиль.")
-    seizures_data = await orm_get_seizures_for_a_specific_period(session, int(current_profile.split('|')[0]), 31)
-    if len(seizures_data) == 0:
+    seizures_data = await orm_get_seizures_for_a_specific_period(session, int(current_profile.split('|')[0]), local_date.year, local_date.month)
+    if len(seizures_data) == 0 or len(seizures_data) < 2:
         return None
+    print(len(seizures_data))
     seizures_with_duration = 0
-    total_duration = 0
+    total_duration_in_sec = 0
     for seizure in seizures_data:
         if seizure.duration:
             seizures_with_duration += 1
-            total_duration += seizure.duration
+            total_duration_in_sec += seizure.duration
     if seizures_with_duration == 0:
         return None
-    return total_duration // seizures_with_duration
+    return get_minutes_and_seconds(total_duration_in_sec // seizures_with_duration)
+
+async def get_avg_days_without_seizures(session: AsyncSession, message: Message):
+    current_profile = await get_cached_current_profile(session, message.chat.id)
+    if current_profile is None:
+        return await message.answer("Выберите профиль.")
+    seizures_data = await orm_get_seizures_by_profile_ascending(session, int(current_profile.split('|')[0]))
+    if len(seizures_data) == 0 or len(seizures_data) < 2:
+        return None
+    date_obj = [datetime.strptime(seizure.date, "%Y-%m-%d") for seizure in seizures_data]
+    total_sum_days = sum(
+        (date_obj[i + 1] - date_obj[i]).days
+        for i in range(len(date_obj) - 1)
+    )
+
+    return total_sum_days // len(date_obj)
