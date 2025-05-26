9f8bbb13e1c64ef55d66ede3bbd7d7f0c6788322
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-05-26 12:10:18 +0700

    delete logic of choosing type of epilepsy

--- handlers/profile_form.py
+++ handlers/profile_form.py
@@ -1,33 +1,27 @@
-import asyncio
-
-from aiogram import Router, F, Bot
-from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State, default_state
-from aiogram.filters import Command, StateFilter
-from sqlalchemy.future import select
+from aiogram.filters import StateFilter
 from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime, timezone
-import pytz
-from timezonefinder import TimezoneFinder
 
 from handlers_logic.states_factories import ProfileForm
 from database.redis_query import set_redis_cached_profiles_list
-from database.models import User, Profile, Drug, profile_drugs
-from database.orm_query import orm_get_user_own_profiles_list, orm_get_user, orm_create_profile, orm_set_current_profile
+from database.models import Profile
+from database.orm_query import orm_get_user_own_profiles_list, orm_get_user
 from lexicon.lexicon import LEXICON_RU
 from keyboards.menu_kb import get_cancel_kb
 from keyboards.profile_form_kb import (
-    get_types_of_epilepsy_kb, get_sex_kb, get_timezone_kb, get_geolocation_for_timezone_kb,
-    get_submit_profile_settings_kb
+    get_ask_for_have_diagnosis_kb, get_sex_kb,
+    get_submit_profile_settings_kb, get_qeustion_about_species
 )
 from services.validators import (
-    validate_name_of_profile_form, validate_age_of_profile_form, validate_list_of_drugs_of_profile_form
+    validate_name_of_profile_form, validate_age_of_profile_form,
+    validate_less_than_40, validate_less_than_30
 )
 from services.redis_cache_data import get_cached_login
 
 profile_form_router = Router()
-
 
 @profile_form_router.callback_query(F.data == "to_filling_profile_form")
 async def start_filling_profile_form(callback: CallbackQuery, state: FSMContext, db: AsyncSession):
@@ -46,36 +40,56 @@ async def start_filling_profile_form(callback: CallbackQuery, state: FSMContext,
 async def process_profile_name(message: Message, state: FSMContext):
     if validate_name_of_profile_form(message.text):
         await state.update_data(profile_name=message.text)
-        await message.answer(LEXICON_RU['enter_type_of_epilepsy'], reply_markup=get_types_of_epilepsy_kb())
-        await state.set_state(ProfileForm.type_of_epilepsy)
+        await message.answer("Вы создаете профиль для человека?", reply_markup=get_qeustion_about_species())
+        await state.set_state(ProfileForm.biological_species)
     else:
         await message.answer(LEXICON_RU['incorrect_profile_name'])
 
-@profile_form_router.callback_query(F.data.in_({'focal_type', 'generalized_type',
-                                    'combied_type','unidentified_type'}),
-                                    StateFilter(ProfileForm.type_of_epilepsy))
-async def process_type_of_epilepsy(callback: CallbackQuery, state: FSMContext):
-    await state.update_data(type_of_epilepsy=callback.data)
-    await callback.message.answer(LEXICON_RU['enter_drugs_info'],
-                                    parse_mode="HTML")
-    await callback.message.answer(LEXICON_RU['enter_drugs'],
-                                reply_markup=get_cancel_kb())
-    await state.set_state(ProfileForm.drugs)
+@profile_form_router.callback_query(F.data == "profile_for_human", StateFilter(ProfileForm.biological_species))
+async def process_about_species(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("У вас есть подтвержденный диагноз?", reply_markup=get_ask_for_have_diagnosis_kb())
+    await state.set_state(ProfileForm.type_of_epilepsy)
     await callback.answer()
 
-@profile_form_router.message(StateFilter(ProfileForm.drugs))
-async def process_drugs(message: Message, state: FSMContext):
-    if validate_list_of_drugs_of_profile_form(message.text) and len(message.text) <= 120:
-        str_of_drugs = str(message.text)
-        await state.update_data(drugs=str_of_drugs)
-        await message.answer(LEXICON_RU['incorrect_age'],
-                            parse_mode="HTML")
+@profile_form_router.callback_query(F.data == "profile_for_animal", StateFilter(ProfileForm.biological_species))
+async def process_about_species(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("Для какого животного вы создаете профиль?", reply_markup=get_cancel_kb())
+    await callback.answer()
+
+@profile_form_router.message(StateFilter(ProfileForm.biological_species))
+async def process_profile_for_animal(message: Message, state: FSMContext):
+    if validate_less_than_30(message.text):
+        await state.update_data(animal_species=message.text)
+        await message.answer("У вас есть подтвержденный диагноз?", reply_markup=get_ask_for_have_diagnosis_kb())
+        await state.set_state(ProfileForm.type_of_epilepsy)
+    else:
+        await message.answer("Вид животного не может быть длиннее 30 симовлов.", reply_markup=get_cancel_kb())
+
+
+@profile_form_router.callback_query(F.data == "have_epilepsy_diagnosis",
+                                    StateFilter(ProfileForm.type_of_epilepsy))
+async def process_users_diagnosis(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("Введите точную формулировку диагноза:",
+                                reply_markup=get_cancel_kb())
+    await callback.answer()
+
+@profile_form_router.callback_query(F.data == "dont_have_epilepsy_diagnosis",
+                                    StateFilter(ProfileForm.type_of_epilepsy))
+async def process_skip_user_diagnosis(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(LEXICON_RU['enter_age'],
+                                reply_markup=get_cancel_kb())
+    await state.set_state(ProfileForm.age)
+    await callback.answer()
+
+@profile_form_router.message(StateFilter(ProfileForm.type_of_epilepsy))
+async def process_diagnosis(message: Message, state: FSMContext):
+    if validate_less_than_40(message.text):
+        await state.update_data(type_of_epilepsy=message.text)
         await message.answer(LEXICON_RU['enter_age'],
-                            reply_markup=get_cancel_kb())
+                                reply_markup=get_cancel_kb())
         await state.set_state(ProfileForm.age)
     else:
-        await message.answer(LEXICON_RU['incorrect_drugs'],
-                            parse_mode='HTML')
+        await message.answer("Длина диагноза не может быть больше 40 симолов")
 
 @profile_form_router.message(StateFilter(ProfileForm.age))
 async def process_age(message: Message, state: FSMContext):
@@ -90,51 +104,15 @@ async def process_age(message: Message, state: FSMContext):
                                     StateFilter(ProfileForm.sex))
 async def process_sex(callback: CallbackQuery, state: FSMContext):
     await state.update_data(sex=callback.data.split('_')[1])
-    await callback.message.answer(LEXICON_RU['timezone_info'],reply_markup=get_geolocation_for_timezone_kb())
-    await callback.message.answer("Выберите ваш часовой пояс или нажмите на кнопку снизу:", reply_markup=get_timezone_kb())
-    await state.set_state(ProfileForm.timezone)
-    await callback.answer()
-
-@profile_form_router.message(F.location, StateFilter(ProfileForm.timezone))
-async def process_timezone_by_geolocation(message: Message, state: FSMContext):
-    tf = TimezoneFinder()
-    latitude = message.location.latitude
-    longitude = message.location.longitude
-    timezone_name = tf.timezone_at(lat=latitude, lng=longitude)
-    if timezone_name:
-        timezone = pytz.timezone(timezone_name)
-        now = datetime.now(timezone)
-        utc_offset_seconds = now.utcoffset().total_seconds()
-        utc_offset_hours = utc_offset_seconds / 3600
-        await state.update_data(timezone=f"{int(utc_offset_hours):+}")
-        data = await state.get_data()
-        profile_info = get_profile_info(data)
-        await message.answer(f"Часовой пояс определен: {int(utc_offset_hours):+}", reply_markup=ReplyKeyboardRemove())
-        await message.answer(f"Анкета профиля заполнена!")
-        await message.answer(f"{profile_info}", parse_mode='HTML')
-        await message.answer("Нажмите 'Подтвердить', чтобы сохранить введенные данные или 'Отменить', чтобы ввести данные снова.",  reply_markup=get_submit_profile_settings_kb())
-        await state.set_state(ProfileForm.check_form)
-    else:
-        await message.answer("Часовой пояс не найден, воспользуйтесь клавиатурой")
-
-
-
-@profile_form_router.callback_query(F.data.contains("timezone_"),
-                                    StateFilter(ProfileForm.timezone))
-async def process_timezone(callback: CallbackQuery, state: FSMContext):
-    await state.update_data(timezone=callback.data.split('_')[1])
     data = await state.get_data()
     profile_info = get_profile_info(data)
-    await callback.message.answer(f"Часовой пояс определен: {callback.data.split('_')[1]}",
-                                    reply_markup=ReplyKeyboardRemove())
     await callback.message.answer(f"Анкета профиля заполнена!")
-    await callback.message.answer(f"{profile_info}", parse_mode="HTML")
+    await callback.message.answer(f"{profile_info}", parse_mode='HTML')
     await callback.message.answer("Нажмите 'Подтвердить', чтобы сохранить введенные данные или 'Отменить', чтобы ввести данные снова.",  reply_markup=get_submit_profile_settings_kb())
     await state.set_state(ProfileForm.check_form)
     await callback.answer()
 
-
-@profile_form_router.callback_query(F.data == "submit_profile_settings")
+@profile_form_router.callback_query(F.data == "submit_profile_settings", StateFilter(ProfileForm.check_form))
 async def finish_filling_profile_data(callback: CallbackQuery, state: FSMContext, db: AsyncSession):
     data = await state.get_data()
     print(f"Полученные данные: {data}")
@@ -151,41 +129,18 @@ async def finish_filling_profile_data(callback: CallbackQuery, state: FSMContext
             await state.clear()
             return
 
-
         new_profile = Profile(
             user_id=user.id,
             profile_name=data["profile_name"],
             type_of_epilepsy=data["type_of_epilepsy"],
             age=int(data["age"]),
             sex=data["sex"],
-            timezone=data["timezone"],
+            biological_species = data.get("animal_species", "Человек"),
             created_at=datetime.now(timezone.utc)
         )
 
         print(f"Создается профиль: {new_profile}")
         db.add(new_profile)
-        await db.flush()
-        profile_id = new_profile.id
-        result = await db.execute(select(Profile).filter(Profile.id == profile_id))
-        profile = result.scalars().first()
-
-        existing_drugs = {drug.name: drug.id for drug in (await db.execute(select(Drug))).scalars()}
-        new_profile_drugs = []
-
-        for drug_name in data["drugs"].lower().strip().split(","):
-            if drug_name in existing_drugs:
-                drug_id = existing_drugs[drug_name.strip()]
-            else:
-                new_drug = Drug(name=drug_name.strip())
-                db.add(new_drug)
-                await db.flush()
-                drug_id = new_drug.id
-                existing_drugs[drug_name.strip()] = drug_id
-
-            new_profile_drugs.append({"profile_id": profile.id, "drug_id": drug_id})
-
-        await db.execute(profile_drugs.insert().values(new_profile_drugs))
-        print("Препараты успешно добавлены к профилю.")
 
         profiles = await orm_get_user_own_profiles_list(db, callback.message.chat.id)
 
@@ -202,16 +157,11 @@ async def finish_filling_profile_data(callback: CallbackQuery, state: FSMContext
     await callback.answer()
 
 def get_profile_info(data: dict[str, str]) -> str:
-    str_epilepsy_type = ""
-
-    if data['type_of_epilepsy'] == "focal_type":
-        str_epilepsy_type = "Фокальная"
-    elif data['type_of_epilepsy'] == "generalized_type":
-        str_epilepsy_type = "Генерализованная"
-    elif data['type_of_epilepsy'] == "combied_type":
-        str_epilepsy_type = "Комбинированная"
-    elif data['type_of_epilepsy'] == "unidentified_type":
-        str_epilepsy_type = "Неопределенного типа"
-    profile_info = f'<u>Имя профиля</u> : <b>{data["profile_name"]}</b>\n<u>Тип эпилепсии</u> : <b>{str_epilepsy_type}</b>\n<u>Принимаемые препараты</u> : <b>{data["drugs"]}</b>\n<u>Возраст</u> : <b>{data["age"]} лет</b> \n<u>Пол</u> : <b>{"Мужской" if data["sex"] == "male" else "Женский"}</b> \n<u>Часовой пояс</u> : <b>{data["timezone"]}</b>'
-
+    profile_info = (
+        f'<u>Имя профиля</u>: <b>{data["profile_name"]}</b>\n'
+        f'<u>Вид диагностированной эпилепсии</u>: <b>{"Неопределенного типа" if data.get("type_of_epilepsy", None) is None else data["type_of_epilepsy"]}</b>\n'
+        f'<u>Возраст</u>: <b>{data["age"]} лет</b> \n'
+        f'<u>Пол</u>: <b>{"Мужской" if data["sex"] == "male" else "Женский"}</b> \n'
+        f'{'<u>Животное</u>: ' + data.get("animal_species", None) if data.get("animal_species", None) is not None else ''}'
+    )
     return profile_info
