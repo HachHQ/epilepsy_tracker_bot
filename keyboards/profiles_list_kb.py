a8d47d0df846d201b2e76d90d5166beaac2826e6
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-05-26 12:19:25 +0700

    add kb for a control submenu

--- keyboards/profiles_list_kb.py
+++ keyboards/profiles_list_kb.py
@@ -1,6 +1,6 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
-from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
-from aiogram.types import InlineKeyboardButton, KeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton
 
 
 def get_paginated_profiles_kb(
@@ -10,18 +10,19 @@ def get_paginated_profiles_kb(
     profile_type: str = "user_own",
     to_share: bool = False
 ) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    if profiles is None:
+        builder.button(text="✏️ Создать профиль", callback_data="to_filling_profile_form")
+        return builder.as_markup()
     total_profiles = len(profiles)
     total_pages = (total_profiles + page_size - 1) // page_size
     if page < 0 or page >= total_pages:
         page = 0
-    builder = InlineKeyboardBuilder()
-    builder.button(text="✏️ Создать профиль", callback_data="to_filling_profile_form")
     start_index = page * page_size
     end_index = start_index + page_size
-    iterator = 0
-    for profile in profiles[start_index:end_index]:
-        iterator+=1
-        builder.button(text=f"{iterator} " + profile["profile_name"],callback_data=f"select_profile:{profile['id']}:{profile['profile_name']}{"|share" if to_share else ""}")
+    builder.button(text="✏️ Создать профиль", callback_data="to_filling_profile_form")
+    for index, profile in enumerate(profiles[start_index:end_index]):
+        builder.button(text=f"{index+1} - " + profile["profile_name"],callback_data=f"select_profile:{profile['id']}:{profile['profile_name']}{"|share" if to_share else ""}")
     builder.adjust(1)
     if total_profiles > page_size:
         nav_buttons = []
@@ -37,5 +38,14 @@ def get_choosing_type_of_profiles_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text="Собственные профили", callback_data="profile_type:user_own")
     builder.button(text="Профили доверенных лиц", callback_data="profile_type:trusted")
+    builder.adjust(1)
+    return builder.as_markup()
+
+def get_profile_submenu_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.button(text="👤 Редактирование профиля", callback_data="prof_edit")
+    builder.button(text="⚙️ Панель управления ДЛ", callback_data="trusted_person_control_panel")
+    builder.button(text="🔗 Добавить ДЛ",callback_data="add_trusted")
+    builder.button(text="↩️ Назад", callback_data="to_menu_edit")
     builder.adjust(1)
     return builder.as_markup()
