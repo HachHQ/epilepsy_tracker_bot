6ed6b40ff4ce8c0e01d29dd45ee0499d7820d232
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-06-01 13:56:00 +0700

    Add classes for medication and notifications

--- database/models.py
+++ database/models.py
@@ -25,6 +25,7 @@ class User(Base):
     telegram_fullname = Column(String(64))
     name = Column(String(25))
     login = Column(String(25), nullable=False, unique=True)
+    timezone = Column(String(3))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     current_profile = Column(Integer, default=None, nullable=True)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False, onupdate=func.now())
@@ -37,15 +38,14 @@ class Profile(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
     profile_name = Column(String(40), nullable=False)
-    type_of_epilepsy = Column(String(20))
+    type_of_epilepsy = Column(String(40))
     age = Column(Integer)
     sex = Column(String(20))
-    timezone = Column(String(3))
+    biological_species = Column(String(30))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False, onupdate=func.now())
 
     user = relationship("User", back_populates="profiles")
-    drugs = relationship("Drug", secondary="profile_drugs", back_populates="profiles")
 
     def to_dict(self):
         return {
@@ -54,18 +54,10 @@ class Profile(Base):
             "type_of_epilepsy": self.type_of_epilepsy,
             "age": self.age,
             "sex": self.sex,
-            "timezone": self.timezone,
+            "biological_species": self.biological_species,
             "created_at": self.created_at.isoformat() if self.created_at else None,
             "updated_at": self.updated_at.isoformat() if self.updated_at else None
         }
-
-class Drug(Base):
-    __tablename__ = 'drugs'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30), nullable=False)
-
-    profiles = relationship("Profile", secondary="profile_drugs", back_populates="drugs")
 
 class Seizure(Base):
     __tablename__ = 'seizures'
@@ -82,7 +74,7 @@ class Seizure(Base):
     video_tg_id = Column(String(255), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
-    #creator_user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    creator_login = Column(String(25), nullable=False)
     #type_of_seizure = Column(String(30), nullable=True)
     #postical_duration = Column(Integer(), nullable=True)
     #postical_symptoms = Column(String(250), nullable=True)
@@ -99,12 +91,13 @@ class TrustedPersonProfiles(Base):
     profile_id = Column(Integer, ForeignKey("profiles.id", ondelete="CASCADE"))
     can_read = Column(Boolean, nullable=False, default=True)
     can_edit = Column(Boolean, nullable=False, default=True)
+    #receives notification = Column(Boolean, nullable=False, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
 class TrustedPersonRequest(Base):
     __tablename__ = "trusted_person_requests"
 
-    id = Column(String, primary_key=True, server_default=func.now())
+    id = Column(String, primary_key=True)
     sender_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     recepient_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     transmitted_profile_id = Column(Integer, ForeignKey("profiles.id", ondelete="CASCADE"), nullable=False)
@@ -113,41 +106,34 @@ class TrustedPersonRequest(Base):
     expires_at = Column(DateTime(timezone=True), server_default=text("NOW() + INTERVAL '10 minutes'"), nullable=False)
 
 '''
+class UserNotifications(Base):
+    __tablename__ = 'user_notifications'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, unique=True)
+    notification_time_hour = Column(Integer, nullable=False, default=9)  # 0-23
+    notification_time_minute = Column(Integer, nullable=False, default=0)  # 0, 15, 30, 45
+    pattern = Column(String(20), nullable=False, default="daily")  # daily, every_2_days, every_3_days
+    is_enabled = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+'''
+
+'''
 class MedicationCourse(Base):
     __tablename__ = 'medication_courses'
 
     id = Column(Integer, primary_key=True)
     profile_id = Column(Integer, ForeignKey('profiles.id', ondelete='CASCADE'), nullable=False)
 
-    # Если препарат выбран из справочника
-    drug_id = Column(Integer, ForeignKey('drugs.id'), nullable=True)
-
-    # Если препарат введен вручную
-    manual_entry_name = Column(String(100), nullable=True)  # Например: "Кеппра 500мг"
-    is_manual = Column(Boolean, default=False)  # True — пользователь ввел руками
-
-    dosage = Column(String(50))       # Например: "100мг 2 раза в день"
-    form = Column(String(30))         # Таблетки, сироп и т.д.
-    frequency = Column(String(30))    # Например: "2 раза в день"
-    time_slots = Column(ARRAY(String), nullable=True)  # Временные точки приема
-    reminder_enabled = Column(Boolean, default=False)
-
-    raw_input = Column(String(200), nullable=True)  # Сырой текст от пользователя
-    notes = Column(String(200), nullable=True)      # Примечания
+    raw_entry_name = Column(String(100))
+    dosage = Column(String(50))
+    frequency = Column(String(30))
+    notes = Column(String(200))
 
     start_date = Column(Date)
     end_date = Column(Date, nullable=True)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
-
-    # Relationships
-    drug = relationship("Drug", back_populates="medication_courses")
-    profile = relationship("Profile", back_populates="medication_courses")'''
-
-profile_drugs = Table(
-    'profile_drugs',
-    Base.metadata,
-    Column('profile_id', Integer, ForeignKey('profiles.id', ondelete="CASCADE"), primary_key=True),
-    Column('drug_id', Integer, ForeignKey('drugs.id', ondelete="CASCADE"), primary_key=True)
-)
+    '''
