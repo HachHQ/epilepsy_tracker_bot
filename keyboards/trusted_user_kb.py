2798cec48a395b6d5f0549eff29878b82a8016c4
Arthur Krutoy <arturkrutoj134@gmail.com>
2025-06-17 15:26:27 +0700

    Add handlers for processing editin permissions of trusted person

--- keyboards/trusted_user_kb.py
+++ keyboards/trusted_user_kb.py
@@ -19,3 +19,21 @@ def get_y_or_n_buttons_to_finish_process() -> InlineKeyboardMarkup:
     builder.button(text="Нет", callback_data="reject_transfer")
     builder.row(cancel_btn)
     return builder.as_markup()
+
+def get_commiting_changing_editing_permission_kb(tpp_id: int):
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Да", callback_data=f"tpchangeediting:yes:{tpp_id}")
+    builder.button(text="Нет", callback_data=f"tpchangeediting:no:{tpp_id}")
+    return builder.as_markup()
+
+def get_commiting_changing_notify_permission_kb(tpp_id: int):
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Да", callback_data=f"tpchangegettingnotify:yes:{tpp_id}")
+    builder.button(text="Нет", callback_data=f"tpchangegettingnotify:no:{tpp_id}")
+    return builder.as_markup()
+
+def get_commiting_deleting_trusted_person_kb(tpp_id: int):
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Да", callback_data=f"tpdeleting:yes:{tpp_id}")
+    builder.button(text="Нет", callback_data=f"tpdeleting:no:{tpp_id}")
+    return builder.as_markup()
